a31fc23ba8d9e33008d8cd6ad28ea9ccfab5ae07
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-10 23:11:22 +0530

    Create 5ifElse.go
    use of if else statement in go lang

---
+++ 5ifElse.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+    var age = 12;
+    
+    if(age > 15){
+        fmt.Print("User can Drink cofee or Milk");
+    }else{
+        fmt.Print("User can only drink Milk");
+    }
+}
+
+
+// using if else statement in go lang
+
