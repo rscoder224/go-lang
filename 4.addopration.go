85bd00d515188e9ecd4a2d19a7c7009fc026714b
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-10 23:06:37 +0530

    Create 4.addopration.go

---
+++ 4.addopration.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+    
+    
+    // performing addition in Go lang
+    
+    var num1 = 12;
+    
+    var num2 = 13;
+    
+    fmt.Println(num1 + num2);
+    
+}
